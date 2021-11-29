09ae2a5916601c0d486868cd45ee0407553bd114
Jayane Pontes <pontesjayane@gmail.com>
2021-11-29 15:44:49 -0300

    Requisitos 7 ao 9

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let pixelBoard = document.querySelector("#pixel-board");
+const pixelBoard = document.querySelector("#pixel-board");
 for (let index = 0; index < 5; index += 1) {
     let linhas = document.createElement("div");
     pixelBoard.appendChild(linhas);
@@ -10,11 +10,49 @@ for (let index = 0; index < 5; index += 1) {
     } 
 }
 
+const cor1 = document.querySelector("#cor1");
+let cor2 = document.querySelector("#cor2");
+let cor3 = document.querySelector("#cor3");
+let cor4 = document.querySelector("#cor4");
+const colorPalet = document.querySelector("#color-palette");
+const botao = document.querySelector("#clear-board");
+const pixel = document.querySelector(".pixel");
 
-
-let corSelecionada = document.querySelector("#color-palette");
-corSelecionada.addEventListener("click" , selecionarCor);
-
-function selecionarCor (corSelecionada) {
-
+function selecionarCor(cor) {
+    let selecionarCor = document.querySelector(".selected");
+    selecionarCor.classList.remove("selected");
+    cor.target.classList.add("selected");
+    console.log(selecionarCor);
+    console.log(cor.target);
+} 
+function pintarPixel(pixel) {
+    let selecionarCor = document.querySelector(".selected");
+    const cliqueiNoPixel = pixel.target;
+    cliqueiNoPixel.id = selecionarCor.id;
+    console.log("Pintando cor selecionada", pixel);
 }
+function clickBotao() {
+    const pixel1 = document.querySelectorAll('.pixel');
+    //pegando a classe.pixel para poder comprar com o tamnho do loop
+    for (let i = 0; i < pixel1.length; i += 1) {
+        //loop no for para adicionar o id white
+        pixel1[i].id = 'white'; // id white add
+    }
+}
+function eventosDeClick() {
+    colorPalet.addEventListener("click", selecionarCor);
+    botao.addEventListener("click", clickBotao);
+    pixelBoard.addEventListener("click", pintarPixel)
+}
+window.onload = function init () {
+    eventosDeClick();
+}
+
+
+
+
+
+
+
+
+
